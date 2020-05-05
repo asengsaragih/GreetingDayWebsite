505574324a9d1d13259e12fc8400329a42b78082
aldi saragih <aldiwahyu.saragih@gmail.com>
2020-05-05 22:20:53 +0700

    fixed

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function getCurrentDay() {
     var nameRaw = getLinkName();
     var name = "";
     var greeting = "";
+    var i = 0;
 
     if (nameRaw == null || nameRaw == "") {
         name = "Tanpa Nama";
@@ -29,20 +30,62 @@ function getCurrentDay() {
 
     if (h >= 5 && h <= 10) {
         greeting = "Good Morning";
+        changeBackgroundAnimation(1);
     } else if (h >= 11 && h <= 15) {
         greeting = "Good Afternoon";
+        changeBackgroundAnimation(1);
     } else if (h >= 16 && h <= 18) {
         greeting = "Good Evening";
+        changeBackgroundAnimation(2);
     } else {
         greeting = "Good Night";
+        changeBackgroundAnimation(2);
     }
 
-    document.getElementById('text-greeting').innerHTML = greeting + " " + capitalizeFirstLetter(name);
+    var textFull = greeting + " " + capitalizeFirstLetter(name);
+    var canvas = document.getElementById('text-greeting');
+
+    var typewriter = new Typewriter(canvas, {
+        delay: 100
+    });
+
+    typewriter.typeString(textFull).start();
 }
 
 function getLinkName() {
     var name = location.search.substring(1);
     return name;
+}
+
+function changeBackgroundAnimation(color) {
+    // 1 for white
+    // 2 for black
+
+    if (color == 1) {
+        $(document).ready(function(){
+            $(".area").css("background", "#ffffff");
+            $(".circles li").css("background", "#4c4c4c");
+
+            $("#text-greeting").css("color", "#000000");
+            $("#text-time").css("color", "#000000");
+            $(".text-footer").css("color", "#000000");
+
+        });
+    } else {
+        $(document).ready(function(){
+            $(".area").css("background", "#000000");
+            $(".circles li").css("background", "#ffffff");
+
+
+            $("#text-greeting").css("color", "#ffffff");
+            $("#text-time").css("color", "#ffffff");
+            $(".text-footer").css("color", "#ffffff");
+        });
+    }
+}
+
+function instagramDirect() {
+    window.location.assign("https://instagram.com/aldi_saragih/");
 }
 
 function capitalizeFirstLetter(string) {
